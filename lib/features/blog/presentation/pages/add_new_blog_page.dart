6838697718a50bea9b8950df44a12a4cfe649c59
Parth Darji <parthdarji2961@gmail.com>
2024-12-12 11:15:35 +0530

    pick image locally done in add new blog page

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,8 +1,10 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blog_app/core/theme/app_pallete.dart';
+import 'package:flutter_blog_app/core/utils/pick_image.dart';
 import 'package:flutter_blog_app/features/blog/presentation/widgets/blog_editor.dart';
 
 class AddNewBlogPage extends StatefulWidget {
@@ -27,6 +29,18 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
     super.dispose();
   }
 
+  File? image;
+
+  void selectImage() async {
+    final pickedImage = await pickImage();
+
+    if (pickedImage != null) {
+      setState(() {
+        image = pickedImage;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,31 +59,49 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
           padding: const EdgeInsets.all(16),
           child: Column(
             children: [
-              DottedBorder(
-                color: AppPallete.borderColor,
-                dashPattern: const [10, 4],
-                radius: const Radius.circular(15),
-                borderType: BorderType.RRect,
-                strokeCap: StrokeCap.round,
-                child: const SizedBox(
-                  height: 150,
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.folder_open,
-                        size: 40,
+              image == null
+                  ? GestureDetector(
+                      onTap: selectImage,
+                      child: DottedBorder(
+                        color: AppPallete.borderColor,
+                        dashPattern: const [10, 4],
+                        radius: const Radius.circular(15),
+                        borderType: BorderType.RRect,
+                        strokeCap: StrokeCap.round,
+                        child: const SizedBox(
+                          height: 150,
+                          width: double.infinity,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                              SizedBox(height: 15),
+                              Text(
+                                "Select your image",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      SizedBox(height: 15),
-                      Text(
-                        "Select your image",
-                        style: TextStyle(fontSize: 15),
+                    )
+                  : GestureDetector(
+                      onTap: selectImage,
+                      child: SizedBox(
+                        height: 150,
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            image!,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
+                    ),
               const SizedBox(height: 20),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
